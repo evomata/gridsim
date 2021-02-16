a88d33df11850e5fd1968e5518b75864baa2335a
Geordon Worley <vadixidav@gmail.com>
2021-02-16 02:36:37 -0500

    remove choosers, they are too specialized for this crate

--- src/direction.rs
+++ src/direction.rs
@@ -21,26 +21,6 @@ pub trait Direction: Copy + From<usize> + Into<usize> {
         Self::directions().count()
     }
 
-    /// Must provide an input iterator with length of the number of directions for the `Direction` impl
-    /// which contains sigmoid outputs in the range of (0.0, 1.0). This will choose a direction based on
-    /// the highest sigmoid output. If none of the values are greater than 0.5, then it will choose no direction.
-    #[inline]
-    fn chooser(sigmoids: impl Iterator<Item = f32>) -> Option<(Self, f32)> {
-        sigmoids
-            .map(float_ord::FloatOrd)
-            .enumerate()
-            .max_by_key(|&(_, n)| n)
-            .and_then(|(ix, value)| (value.0 > 0.5).then(|| (ix.into(), value.0)))
-    }
-
-    /// Must provide an input slice with length of the number of directions for the `Direction` impl
-    /// which contains sigmoid outputs in the range of (0.0, 1.0). This will choose a direction based on
-    /// the highest sigmoid output. If none of the values are greater than 0.5, then it will choose no direction.
-    #[inline]
-    fn chooser_slice(sigmoids: &[f32]) -> Option<(Self, f32)> {
-        Self::chooser(sigmoids.iter().copied())
-    }
-
     #[inline]
     fn turn_clockwise(self) -> Self {
         let mut n: usize = self.into();
